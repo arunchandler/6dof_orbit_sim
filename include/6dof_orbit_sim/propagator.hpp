9d47b0157778bd9700a50967278147cafdc19475
Arun Chandler <schand2@g.clemson.edu>
2026-05-11 10:55:07 -0400

    added propagation for translational state and attitude state

--- include/6dof_orbit_sim/propagator.hpp
+++ include/6dof_orbit_sim/propagator.hpp
@@ -17,11 +17,23 @@ SixDoFStateMat propagate6DoF(const SixDoFState& initial_state, Real t0, Real tf,
 }
 
 TranslationalStateMat propagateTranslationalState(const ECIState& initial_state, Real t0, Real tf, Real dt, DerivFunc deriv_func) {
-
+    int steps = static_cast<int>((tf - t0) / dt) + 1;
+    TranslationalStateMat states(TRANLATIONAL_STATE_DIM, steps);
+    states.col(0) = packTranslational(initial_state);
+    for (int i = 1; i < steps; ++i) {
+        states.col(i) = rk4Step(deriv_func, t0 + (i-1)*dt, states.col(i-1), dt);
+    }
+    return states;
 }
 
 AttitudeStateMat propagateAttitudeState(const AttitudeState& initial_state, Real t0, Real tf, Real dt, DerivFunc deriv_func) {
-
+    int steps = static_cast<int>((tf - t0) / dt) + 1;
+    AttitudeStateMat states(ATTITUDE_STATE_DIM, steps);
+    states.col(0) = packAttitude(initial_state);
+    for (int i = 1; i < steps; ++i) {
+        states.col(i) = rk4Step(deriv_func, t0 + (i-1)*dt, states.col(i-1), dt);
+    }
+    return states;
 }
 
 } // namespace orb
